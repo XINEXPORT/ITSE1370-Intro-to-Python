ad6dadde302c7486b89d76eb57a69159c8838ee4
Christine Hoang <christinehoangt@gmail.com>
2024-05-15 10:42:43 -0500

    basicio and helloworld added

---
+++ introtopython/basicio.py
@@ -0,0 +1,12 @@
+
+integer = int(input("Enter integer: "))
+print("You entered:", integer)
+foursqr = pow(integer, 2)
+print(integer, "squared is", foursqr)
+cubed = pow(integer, 3)
+print("And", integer, "cubed is", cubed )
+integerother = int(input("Eneter another integer: "))
+total = integer + integerother
+print(integer, "+", integerother, "is", total)
+totalmul = integer * integerother
+print(integer, "+", integerother, "is", totalmul)
