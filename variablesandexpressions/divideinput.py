8269f38a7572ff21c51b34295694e5640e52261b
Christine Hoang <christinehoangt@gmail.com>
2024-05-16 21:21:55 -0500

    added float.py

--- variablesandexpressions/divideinput.py
+++ variablesandexpressions/divideinput.py
@@ -15,3 +15,4 @@ for i in range(3):
 
 
 
+
---
+++ variablesandexpressions/float.py
@@ -0,0 +1,18 @@
+print(f'{(7.0/3.0):.2f}')
+print(f'{0.125:.1f}')
+print(f'{9.1357:.3f}')
+
+wall_area = float(input('wall area: '))
+gallons_needed = wall_area / 350.0
+print(f"{gallons_needed:.5f}")
+
+#the input is 0.240
+#the output is 4.167
+force_input = float(input())
+inverse_force = 1 / force_input
+print(inverse_force)
+
+
+
+
+
