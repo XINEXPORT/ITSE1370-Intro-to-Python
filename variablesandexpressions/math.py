e685df0ab00f6538b414fa5763ae8cda9f3080ae
Christine Hoang <christinehoangt@gmail.com>
2024-05-19 21:45:18 -0500

    math.py added

---
+++ variablesandexpressions/math.py
@@ -0,0 +1,19 @@
+import math
+
+x = math.pow(4.0, math.sqrt(4.0))
+print(x)
+
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+
+print_dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(f'Points distance: {point_dist}')
+
+
+p = float(input())
+q = float(input())
+
+r = math.sqrt((p + q)**3)
+print(f'{r:.1f}')
