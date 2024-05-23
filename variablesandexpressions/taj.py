8ca1bc596b71f764b46f4980ca0da477ba3d9815
Christine Hoang <christinehoangt@gmail.com>
2024-05-22 21:20:23 -0500

    taj.py added

---
+++ variablesandexpressions/taj.py
@@ -0,0 +1,44 @@
+"""String name2_input is read from input. String str1 contains a special character.
+Modify str1 so the program outputs Taj doesn't know followed by name2_input.
+Ex: If the input is Hall, then the output is:
+Taj doesn't know Hall"""
+
+
+name2_input = input()
+
+str1 = "Taj doesn't know"
+
+print(str1, name2_input)   
+
+
+fruit_input = input()
+
+shopping_list = '\npomegranates\nraspberries\noranges\npears'
+
+print(fruit_input, end='')
+print(shopping_list)
+
+
+fruit_input = input()
+
+my_list = "Kai's fruits:\nPlums\tPeaches "
+
+print(my_list, end='')
+print(fruit_input)
+
+print("The dog's name is \"Colby.\"")	
+print('Beth\nDrew')	
+print('Maya\nAlex\\\nDrew\\nArya')	
+print(r'The escape sequence for double quote is \"')	
+
+
+
+
+
+
+
+
+
+
+
+
