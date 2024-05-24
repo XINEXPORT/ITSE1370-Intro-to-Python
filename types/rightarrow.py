5d566df158c3c8779bcd8c5fdee6c04b74bf0087
Christine Hoang <christinehoangt@gmail.com>
2024-05-23 22:03:29 -0500

    rightarrow.py

---
+++ types/rightarrow.py
@@ -0,0 +1,19 @@
+#       #
+# ******##
+# ******###
+# ******##
+#       #
+
+base_char = input()
+head_char = input()
+
+row1 = '      ' + head_char
+row2 = ' ' * 6 + base_char + head_char
+row3 = ' ' * 5 + base_char + head_char + base_char
+
+print(row1)
+print(row2)
+print(row3)
+print(row2)
+print(row1)
+
