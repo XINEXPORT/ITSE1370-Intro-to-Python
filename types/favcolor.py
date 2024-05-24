e9e6cbd587fb6b2b0f7544e428294c318b052869
Christine Hoang <christinehoangt@gmail.com>
2024-05-24 10:00:21 -0500

    favcolor added

---
+++ types/favcolor.py
@@ -0,0 +1,9 @@
+
+fav_color = "khaki"
+least_fav_color = input()
+
+a = f"My favorite color {fav_color} has {len(fav_color)} characters."
+b = f"My least favorite color {least_fav_color} has {len(least_fav_color)} characters."
+
+print(a)
+print(b)
