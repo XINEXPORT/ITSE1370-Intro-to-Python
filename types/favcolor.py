054b94a4dca532a43c84baf280e106551a954199
Christine Hoang <christinehoangt@gmail.com>
2024-05-24 10:07:51 -0500

    added fav fruit

--- types/favcolor.py
+++ types/favcolor.py
@@ -1,4 +1,3 @@
-
 fav_color = "khaki"
 least_fav_color = input()
 
@@ -7,3 +6,15 @@ b = f"My least favorite color {least_fav_color} has {len(least_fav_color)} chara
 
 print(a)
 print(b)
+
+#######################
+fav_fruit = input()
+char_index1 = int(input())
+char_index2 = int(input())
+char1 = fav_fruit[char_index1]
+char2 = fav_fruit[char_index2]
+
+
+print('Fruit chosen:', fav_fruit)
+print('Character at index', char_index1, 'is', char1)
+print('Character at index', char_index2, 'is', char2)
