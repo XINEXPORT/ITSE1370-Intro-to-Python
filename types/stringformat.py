3d25a339ad2adefcdec0bef8b57abb5b827a2e09
Christine Hoang <christinehoangt@gmail.com>
2024-05-27 07:30:47 -0500

    stringformat added

---
+++ types/stringformat.py
@@ -0,0 +1,28 @@
+
+output = f'{2*2}'
+output = f'{2*2=}'
+
+kids = 4
+adults = 2
+output = f'{kids+adults} total'
+
+kids = 4
+adults = 2
+output = f'{kids+adults=} total'
+
+output = f'{{2}} + {{3}} = {{5}}'
+
+print(output)
+
+ #####################
+ 
+number = 10
+
+print(f'{number:.2f}, {number:d}, {number:03d}')
+
+ #####################
+
+first_name = input()
+last_name = input()
+
+print(f"{first_name}, {last_name}")
