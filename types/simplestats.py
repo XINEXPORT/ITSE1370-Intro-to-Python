595107a198a70068619b7369479b22a3dc97265a
Christine Hoang <christinehoangt@gmail.com>
2024-05-27 16:06:38 -0500

    saving

--- types/simplestats.py
+++ types/simplestats.py
@@ -1,21 +1,21 @@
-# """Given 4 floating-point numbers. Use a string formatting expression with conversion specifiers to output their product 
-# and their average as integers (rounded), then as floating-point numbers.
+"""Given 4 floating-point numbers. Use a string formatting expression with conversion specifiers to output their product 
+and their average as integers (rounded), then as floating-point numbers.
 
-# Output each rounded integer using the following:
-# print(f'{your_value:.0f}')
-# Output each floating-point value with three digits after the decimal point, which can be achieved as follows:
-# print(f'{your_value:.3f}')
+Output each rounded integer using the following:
+print(f'{your_value:.0f}')
+Output each floating-point value with three digits after the decimal point, which can be achieved as follows:
+print(f'{your_value:.3f}')
 
-# input is
-# 8.3
-# 10.4
-# 5.0
-# 4.8
+input is
+8.3
+10.4
+5.0
+4.8
 
-# output is
-# 2072 7
-# 2071.680 7.125
-# """
+output is
+2072 7
+2071.680 7.125
+"""
 num1 = float(input())
 num2 = float(input())
 num3 = float(input())
