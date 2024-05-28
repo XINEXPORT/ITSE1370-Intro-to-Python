27fec20a0693e24b46f97b50a72de705850f9b54
Christine Hoang <christinehoangt@gmail.com>
2024-05-27 22:35:34 -0500

    right arrow completed

--- types/rightarrow.py
+++ types/rightarrow.py
@@ -11,12 +11,14 @@ base_char = input()
 head_char = input()
 
 row1 = '      ' + head_char
-row2 = ' ' * 6 + base_char + head_char
-row3 = ' ' * 5 + base_char + head_char + base_char
+row2 = base_char * 6 + head_char * 2
+row3 = base_char * 6 + head_char * 3
+row4 = base_char * 6 + head_char * 2
+row5 = '      ' + head_char
 
 print(row1)
 print(row2)
 print(row3)
-print(row2)
-print(row1)
+print(row4)
+print(row5)
 
--- types/timelog.py
+++ types/timelog.py
@@ -1,24 +1,35 @@
-day1_hours = int(input())
-day2_hours = int(input())
+# day1_hours = int(input())
+# day2_hours = int(input())
 
-time_log = {'Sunday': day1_hours,
-            'Monday': day2_hours
-            }
+# time_log = {'Sunday': day1_hours,
+#             'Monday': day2_hours
+#             }
 
-print(f"Sunday: {time_log['Sunday']}")
-print(f"Monday: {time_log['Monday']}")
+# print(f"Sunday: {time_log['Sunday']}")
+# print(f"Monday: {time_log['Monday']}")
+
+# #########
+
+# auth_code = int(input())
+
+# contact_dict = {765472: 'Tia@milkshakes.org', 
+#                 271913: 'Val@mushrooms.com', 
+#                 546542: 'Gil@cookies.org'}
+
+# if auth_code in contact_dict:
+#     email_address = contact_dict.pop(auth_code)
+#     print(email_address)
+
+# print('Remaining pairs:')
+# print(contact_dict)
 
 #########
 
-auth_code = int(input())
+scores_dict = {'Huy': 59, 'Ken': 38}
+print('Original:')
+print(scores_dict)
 
-contact_dict = {765472: 'Tia@milkshakes.org', 
-                271913: 'Val@mushrooms.com', 
-                546542: 'Gil@cookies.org'}
 
-if auth_code in contact_dict:
-    email_address = contact_dict.pop(auth_code)
-    print(email_address)
 
-print('Remaining pairs:')
-print(contact_dict)
+print('Updated:')
+print(scores_dict)
