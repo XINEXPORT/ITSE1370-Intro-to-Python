f14b2f1edf2849c97800ae291096bdc2b2395add
Christine Hoang <christinehoangt@gmail.com>
2024-05-30 22:00:17 -0500

    productivity.py added

---
+++ branching/homework.py
@@ -0,0 +1,18 @@
+num_minutes = int(input())
+
+if (num_minutes > 55):
+    print('I can finish my homework and have extra time')
+elif (num_minutes > 11):
+    print('I can finish at least a third of my homework')
+elif (num_minutes <= 6):
+    print('I do not have time to do my homework')
+    
+#############################
+
+user_num = int(input()) 
+
+if user_num == 2:
+    print('Num is equal to two')
+else:
+    print('Num is not two')
+
---
+++ branching/productivity.py
@@ -0,0 +1,29 @@
+productivity_available = int(input())
+
+if (productivity_available >= 25):
+    print('I can run a mile.')
+if (productivity_available > 60):
+    print('I can stay up all night.')
+elif (productivity_available <= 4):
+    print('I am tired.')
+    
+#################
+
+num_score1 = int(input())
+num_score2 = int(input())
+
+if (num_score1 > 20):
+    print('num_score1 is greater than 20.')
+if (num_score2 <= 30):
+    num_score2 = 1;
+else:
+    print('num_score2 is greater than 30.')
+
+print(f'num_score2 is {num_score2}.')
+
+##################
+
+num_coconuts = int(input())
+dollar_amount = int(input())
+
+
