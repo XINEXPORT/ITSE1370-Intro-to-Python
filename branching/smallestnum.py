760dee0ae7798dc3aa01577e4b9c9d0b1bf467f7
Christine Hoang <christinehoangt@gmail.com>
2024-05-31 22:28:33 -0500

    smallestnum added and done

---
+++ branching/smallestnum.py
@@ -0,0 +1,15 @@
+# Write a program whose inputs are three integers, and whose output is the smallest of the three values.
+
+# Ex: If the input is:
+# 7
+# 15
+# 3
+# the output is: 3
+
+x = int(input())
+y = int(input())
+z = int(input())
+
+minimum = min(x,y,z)
+
+print(minimum)
