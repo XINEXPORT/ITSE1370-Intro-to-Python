ca723355cd1ab1380e3f5be6ddd2e2f9d5cf5925
Christine Hoang <christinehoangt@gmail.com>
2024-06-04 21:03:29 -0500

    count added

---
+++ loops/count.py
@@ -0,0 +1,16 @@
+year = 1792
+current_year = 2024
+
+while year <= current_year:
+    print(year)
+    year = year + 4
+    
+#####
+
+target = int(input())
+n = int(input())
+step = 4
+while n <= target:
+    print(n * 2)
+    n += step
+
