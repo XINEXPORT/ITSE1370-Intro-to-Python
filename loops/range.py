e3005e78a57720e0b9d108fe5f5bdd471aa3d22c
Christine Hoang <christinehoangt@gmail.com>
2024-06-05 21:21:23 -0500

    save

--- loops/range.py
+++ loops/range.py
@@ -1,11 +1,30 @@
 seconds_max = int(input())
 
-print('Seconds lapsed:', end=' ')
+# print('Seconds lapsed:', end=' ')
 
-for i in range(0, seconds_max + 1):
+# for i in range(0, seconds_max + 1):
+#     print(i, end=' ')
+# print()
+
+###########
+
+address_minimum = int(input())
+address_maximum = int(input())
+
+print('House numbers:', end=' ')
+
+for i in range(address_minimum, address_maximum):
     print(i, end=' ')
 print()
 
 ############
 
+first_signal = int(input())
+last_signal = int(input())
 
+print('Signals received:', end=' ')
+
+for curr_signal in range(first_signal, last_signal+1):
+    if curr_signal % 2 == 0:
+        print(curr_signal, end=' ')
+print()
