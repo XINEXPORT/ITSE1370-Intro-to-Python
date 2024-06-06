f75fd0e2246ef403e7f02448a07bac82aedba054
Christine Hoang <christinehoangt@gmail.com>
2024-06-05 21:23:03 -0500

    save

--- loops/range.py
+++ loops/range.py
@@ -1,10 +1,10 @@
 seconds_max = int(input())
 
-# print('Seconds lapsed:', end=' ')
+print('Seconds lapsed:', end=' ')
 
-# for i in range(0, seconds_max + 1):
-#     print(i, end=' ')
-# print()
+for i in range(0, seconds_max + 1):
+    print(i, end=' ')
+print()
 
 ###########
 
