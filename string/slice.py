8723430861a6973fcc70f69b4a957e52617e0b04
Christine Hoang <christinehoangt@gmail.com>
2024-06-26 22:40:06 -0500

    strings

--- string/slice.py
+++ string/slice.py
@@ -20,3 +20,37 @@ my_string = input()
 half = len(my_string)//2
 sub_lyric = my_string[half::2]
 print(sub_lyric)
+
+
+address = 'www.yahoo.com'
+
+separator = '/'
+address_tokens = address.split('.')
+print(separator.join(address_tokens))
+
+item_name = input()
+
+print(f'{item_name:*^13}')
+print(f'{item_name:*<13}')
+
+############
+
+
+def print_points(name, age, total_points):
+    print(f'{name} is {age}')
+    print(f'{name} made {total_points} points')
+
+user_name = 'Ron'
+user_age = 23
+regular_time_points = 24
+overtime_points = 6
+
+print_points(user_name, user_age, regular_time_points + overtime_points)
+
+def show(a, b=1, c=4):
+    print(f'{c}/{a}/{b}')
+
+show(c=8, a=5)
+show(b=2, a=7)
+
+
